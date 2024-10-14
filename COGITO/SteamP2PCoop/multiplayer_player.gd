7b341f7920b21daab78a6c847778e736c37dcc32
ac-arcana <acarcana@gmail.com>
2024-10-13 22:02:30 -0700

    Player rotation is synced

--- COGITO/SteamP2PCoop/multiplayer_player.gd
+++ COGITO/SteamP2PCoop/multiplayer_player.gd
@@ -3,9 +3,13 @@ extends CogitoPlayer
 
 @export var sync_weight : float = 0.2
 
-var sync_position := Vector3.ZERO
-var sync_rotation := Vector3.ZERO
-var sync_velocity := Vector3.ZERO
+@export_group("Synced Vars")
+## Exported so the Multiplayer Synchronizer can see the variable
+@export var sync_position := Vector3.ZERO
+## Exported so the Multiplayer Synchronizer can see the variable
+@export var sync_rotation := Quaternion.IDENTITY
+## Exported so the Multiplayer Synchronizer can see the variable
+@export var sync_velocity := Vector3.ZERO
 
 func _enter_tree():
 	## when the game is starting single player mode we don't want to lose authority
@@ -42,18 +46,20 @@ func _physics_process(delta):
 
 func _owner_sync():
 	sync_position = global_position
-	sync_rotation = global_rotation
+	## get_rotation_quaternion prevents an error from sending basis as a quaternion.
+	sync_rotation = body.global_basis.get_rotation_quaternion()
 	sync_velocity = velocity
 
 func _client_sync():
 	global_position = global_position.lerp(sync_position, sync_weight)
-	global_rotation = global_rotation.lerp(sync_rotation, sync_weight)
+	body.global_basis = body.global_basis.slerp(sync_rotation, sync_weight)
+	print(body.global_rotation)
 	velocity = sync_velocity
 	move_and_slide()
 
 
 func _disable_local_playermodel():
-	var player_model = $PlayerModel
+	var player_model = find_child("PlayerModel")
 	if is_multiplayer_authority():
 		player_model.visible = false
 	else:
