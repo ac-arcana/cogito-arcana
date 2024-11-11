7e0cbef987d4b97de6414dc049a26a1915a6bebb
ac-arcana <acarcana@gmail.com>
2024-11-10 18:00:05 -0800

    Synchronize charge of dropped flashlight

--- addons/cogito/SteamP2PCoop/Synchronizers/cogito_drop_synchronizer.gd
+++ addons/cogito/SteamP2PCoop/Synchronizers/cogito_drop_synchronizer.gd
@@ -1,32 +1,58 @@
 class_name CogitoDropSynchronizer
 extends Node
 
+## This slot data does not need to contain any information
+## It is simply an instance, so that we can use it to spoof information from dropped items
+@export var dummy_slot : InventorySlotPD
+
 var local_player : MultiplayerPlayer
+var player_hud : CogitoPlayerHudManager
+
+#TODO handle synchronization when a new player joins after an item has been dropped.
 
 #gets a signal from the multiplayer_player_spawner
 func _on_multiplayer_player_spawner_local_player_spawned(player : MultiplayerPlayer):
 	var inventory_interface = player.find_child("InventoryInterface")
 	inventory_interface.drop_slot_data.connect(_on_drop_slot_data)
 	local_player = player
+	player_hud = local_player.find_child("Player_HUD")
 
 func _on_drop_slot_data(slot_data : InventorySlotPD):
-	print(local_player.name)
-	_rpc_on_drop.rpc(slot_data.inventory_item.resource_path, local_player.player_interaction_component.get_interaction_raycast_tip(local_player.find_child("Player_HUD").item_drop_distance_offset))
+	var quantity : int = 0
+	if slot_data.inventory_item is WieldableItemPD:
+		quantity = slot_data.inventory_item.charge_current
+	elif slot_data.inventory_item is InventoryItemPD:
+		quantity = slot_data.inventory_item.quantity
+	_rpc_on_drop.rpc(slot_data.inventory_item.resource_path, slot_data.quantity, quantity, slot_data.MAX_STACK_SIZE, slot_data.origin_index, local_player.player_interaction_component.get_interaction_raycast_tip(player_hud.item_drop_distance_offset))
 
 
 
 @rpc("any_peer", "call_remote", "reliable")
-func _rpc_on_drop(inventory_item_path : String, drop_position : Vector3):
+func _rpc_on_drop(inventory_item_path : String, slot_quantity : int, item_quantity : int, max_stack_size : int, origin_index : int, drop_position : Vector3):
 	var inventory_item = load(inventory_item_path)
+	## Duplicate to prevent overwriting quantity of item in other player's scenes
+	inventory_item = inventory_item.duplicate()
+	if inventory_item is WieldableItemPD:
+		inventory_item.charge_current = item_quantity
+	elif inventory_item is InventoryItemPD:
+		inventory_item.quantity = item_quantity
 	print("Received Item Drop RPC: " + inventory_item.name)
+	
+	## set up the slot data
+	var slot_data : InventorySlotPD = dummy_slot.duplicate()
+	slot_data.inventory_item = inventory_item
+	slot_data.quantity = slot_quantity
+	slot_data.origin_index = origin_index
+	
 	var scene_to_drop = load(inventory_item.drop_scene)
 	Audio.play_sound(inventory_item.sound_drop)
 	var dropped_item = scene_to_drop.instantiate()
 	dropped_item.position = drop_position
+
+	# TODO send a signal to pickup synchronizer so the dropped item is added and synced
 	dropped_item.find_interaction_nodes()
-	## Oh no, I need all the slot data too!
-	#for node in dropped_item.interaction_nodes:
-	#	if node.has_method("get_item_type"):
-	#		node.slot_data = slot_data
+	for node in dropped_item.interaction_nodes:
+		if node.has_method("get_item_type"):
+			node.slot_data = slot_data
 	
 	CogitoSceneManager._current_scene_root_node.add_child(dropped_item)
